abe8c72b787062e992776f2fcdcee96a4f247796
ytkuo <hahaeating1@gmail.com>
2022-04-05 11:40:47 -0400

    test

---
+++ src/main/java/com/heroku/springbootherokuapp/SpringbootHerokuAppApplication.java
@@ -0,0 +1,15 @@
+package com.heroku.springbootherokuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource(("classpath:application-${spring.profiles.active:default}.properties"))
+public class SpringbootHerokuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHerokuAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heroku/springbootherokuapp/SpringbootHerokuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heroku.springbootherokuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHerokuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
